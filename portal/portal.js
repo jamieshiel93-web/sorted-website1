01a071dfdda8838f4c81da74ac0b9d64e2838004
jamieshiel93-web <jamieshiel93@gmail.com>
2026-07-05 21:29:27 +0100

    Portal update

--- portal/portal.js
+++ portal/portal.js
@@ -1,4 +1,5 @@
 import { auth } from "../firebase.js";
+
 import {
 signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
@@ -6,21 +7,17 @@ signInWithEmailAndPassword
 window.login = function(){
 
 const email = document.getElementById("email").value.trim();
-
 const password = document.getElementById("password").value;
+const error = document.getElementById("error");
+
+error.style.display = "none";
 
 signInWithEmailAndPassword(auth,email,password)
-
-.then(() =>{
-
+.then(()=>{
 window.location.href="dashboard.html";
-
 })
-
 .catch(()=>{
-
-document.getElementById("error").style.display="block";
-
+error.style.display="block";
 });
 
-}
+};
