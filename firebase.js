c82a0b96af074e452a9dd4504b2db84248746f8c
jamieshiel93-web <jamieshiel93@gmail.com>
2026-08-29 05:33:14 +0100

    Firebase update

--- firebase.js
+++ firebase.js
@@ -1,27 +1,63 @@
 // Firebase App
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
 
 // Firebase Authentication
 import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
+
 // Firestore Database
 import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+
+// Firebase Storage
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBUp527nMtmDU9Lf8_EjU2RPjYgI8Kymy0",
-  authDomain: "sorted-client-portal.firebaseapp.com",
-  projectId: "sorted-client-portal",
-  storageBucket: "sorted-client-portal.firebasestorage.app",
-  messagingSenderId: "158256495111",
-  appId: "1:158256495111:web:96cf1f4ad77ff0da74c8a2",
-  measurementId: "G-YLM1F8NL3B"
+
+  apiKey:
+    "AIzaSyBUp527nMtmDU9Lf8_EjU2RPjYgI8Kymy0",
+
+  authDomain:
+    "sorted-client-portal.firebaseapp.com",
+
+  projectId:
+    "sorted-client-portal",
+
+  storageBucket:
+    "sorted-client-portal.firebasestorage.app",
+
+  messagingSenderId:
+    "158256495111",
+
+  appId:
+    "1:158256495111:web:96cf1f4ad77ff0da74c8a2",
+
+  measurementId:
+    "G-YLM1F8NL3B"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app =
+  initializeApp(firebaseConfig);
+
+
+// Firebase services
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
+
+export const storage =
+  getStorage(app);
